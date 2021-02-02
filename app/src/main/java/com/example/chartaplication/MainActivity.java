76b9956b4c47bc71290a971c7eb25678510859a3
Johan Winarto <johanpbrc@gmail.com>
2021-02-02 13:08:20 +0700

    Mengistall Plugin Chart & Membuat Bar Chart

--- app/src/main/java/com/example/chartaplication/MainActivity.java
+++ app/src/main/java/com/example/chartaplication/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.chartaplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), PieChart.class));
+            }
+        });
+
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), BarChartActivity.class));
+            }
+        });
+
+        findViewById(R.id.button3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), LineChart.class));
+            }
+        });
     }
 }
