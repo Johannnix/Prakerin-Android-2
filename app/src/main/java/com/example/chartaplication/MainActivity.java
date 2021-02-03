37483940182c17292d0b0b343d1963d1e5bbeb47
Johan Winarto <johanpbrc@gmail.com>
2021-02-03 13:13:20 +0700

    Berhasil Menggabungkan Chart Pie dan Bar Ke activity_main.xml

--- app/src/main/java/com/example/chartaplication/MainActivity.java
+++ app/src/main/java/com/example/chartaplication/MainActivity.java
@@ -7,12 +7,12 @@ import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.PieData;
-import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.utils.ColorTemplate;
 import java.util.ArrayList;
 import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,10 +34,32 @@ public class MainActivity extends AppCompatActivity {
         BarDataSet bardataset = new BarDataSet(Pembeli, "Pembeli");
         bardataset.setColors(ColorTemplate.COLORFUL_COLORS);
         bardataset.setValueTextColor(Color.BLACK);
-        bardataset.setValueTextSize(16f);
+        bardataset.setValueTextSize(14f);
 
         BarData barData = new BarData(bardataset);
         chart.setData(barData);
         chart.animateY(2000);
+
+        PieChart pieChart = findViewById(R.id.piechart);
+        ArrayList<PieEntry> Produser = new ArrayList<>();
+        Produser.add(new PieEntry(420, 2014));
+        Produser.add(new PieEntry(475, 2015));
+        Produser.add(new PieEntry(508, 2016));
+        Produser.add(new PieEntry(660, 2017));
+        Produser.add(new PieEntry(550, 2018));
+        Produser.add(new PieEntry(530, 2019));
+        Produser.add(new PieEntry(470, 2020));
+
+        PieDataSet pieDataSet = new PieDataSet(Produser, "Pembeli");
+        pieDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        pieDataSet.setValueTextColor(Color.BLACK);
+        pieDataSet.setValueTextSize(14f);
+
+        PieData pieData = new PieData(pieDataSet);
+        pieChart.setData(pieData);
+        pieChart.getDescription().setEnabled(false);
+        pieChart.setCenterText("Pembeli");
+        pieChart.animate();
+
     }
 }
