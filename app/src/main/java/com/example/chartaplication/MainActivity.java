71ab683d2db457a15e5d2102c9e96a70a9b4dbc6
Johan Winarto <johanpbrc@gmail.com>
2021-02-03 12:37:23 +0700

    Mencoba Menggabungkan Chart Ke activity_main.xml

--- app/src/main/java/com/example/chartaplication/MainActivity.java
+++ app/src/main/java/com/example/chartaplication/MainActivity.java
@@ -1,10 +1,18 @@
 package com.example.chartaplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import java.util.ArrayList;
+import com.github.mikephil.charting.charts.PieChart;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,26 +20,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BarChart chart = findViewById(R.id.barchart);
 
-        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), PieChart.class));
-            }
-        });
+        ArrayList Pembeli = new ArrayList();
+        Pembeli.add(new BarEntry(2014, 420));
+        Pembeli.add(new BarEntry(2015, 475));
+        Pembeli.add(new BarEntry(2016, 508));
+        Pembeli.add(new BarEntry(2017, 660));
+        Pembeli.add(new BarEntry(2018, 550));
+        Pembeli.add(new BarEntry(2019, 630));
+        Pembeli.add(new BarEntry(2020, 470));
 
-        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), BarChartActivity.class));
-            }
-        });
+        BarDataSet bardataset = new BarDataSet(Pembeli, "Pembeli");
+        bardataset.setColors(ColorTemplate.COLORFUL_COLORS);
+        bardataset.setValueTextColor(Color.BLACK);
+        bardataset.setValueTextSize(16f);
 
-        findViewById(R.id.button3).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(), LineChart.class));
-            }
-        });
+        BarData barData = new BarData(bardataset);
+        chart.setData(barData);
+        chart.animateY(2000);
     }
 }
