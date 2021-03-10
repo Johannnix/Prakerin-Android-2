dcbb2c74e6acb4353d2a9c30c2713e9153f83687
Johan Winarto <johanpbrc@gmail.com>
2021-03-10 16:09:26 +0700

    Memperbaiki Tampilan

--- app/src/main/java/com/example/chartaplication/MainActivity.java
+++ app/src/main/java/com/example/chartaplication/MainActivity.java
@@ -1,58 +1,52 @@
 package com.example.chartaplication;
 
 import android.app.ProgressDialog;
-
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.charts.PieChart;
-import com.github.mikephil.charting.data.BarDataSet;
-import com.github.mikephil.charting.data.BarEntry;
-import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
+import org.eazegraph.lib.models.PieModel;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+    final int[] MY_COLORS = {Color.parseColor("#FFB259"), Color.parseColor("#ff5959"), Color.parseColor("#43d79b"), Color.parseColor("#4db5ff")};
     private String[] Item = {"Choose Chart","Pie Chart","Bar Chart","Line Chart"};
-    private TextView txtName,txtCases,txtDeaths,txtRecovered,txtActive;
+    private TextView txtName,txtCases,txtDeaths,txtRecovered,txtActive,tgl;
     ProgressDialog dialog;
     LineChart lineChart;
+    PieChart pieChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pieChart = findViewById(R.id.piechart);
 
 //        txtName = findViewById(R.id.txtName);
-//        txtCases = findViewById(R.id.txtCases);
-//        txtDeaths = findViewById(R.id.txtDeaths);
-//        txtRecovered = findViewById(R.id.txtRecovered);
-//        txtActive = findViewById(R.id.txtActive);
+        txtCases = findViewById(R.id.txtCases);
+        txtDeaths = findViewById(R.id.txtDeaths);
+        txtRecovered = findViewById(R.id.txtRecovered);
+        txtActive = findViewById(R.id.txtActive);
+        tgl = findViewById(R.id.tgl);
 
         dialog = new ProgressDialog(this);
         dialog.setMessage("Loading");
@@ -61,6 +55,15 @@ public class MainActivity extends AppCompatActivity {
 
         getData();
 
+    }
+
+    private String getDate(long milliSecond){
+        //Mon, 23 Mar 2020 02:01:04 PM
+        SimpleDateFormat formatter = new SimpleDateFormat("EEE, dd MMMM yyyy hh:mm:ss aaa");
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(milliSecond);
+        return formatter.format(calendar.getTime());
     }
 
     private void getData() {
@@ -72,25 +75,26 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     JSONObject jsonObject = new JSONObject(response.toString());
 //                    txtName.setText(jsonObject.getString("country"));
-//                    txtCases.setText(jsonObject.getString("cases"));
-//                    txtDeaths.setText(jsonObject.getString("deaths"));
-//                    txtRecovered.setText(jsonObject.getString("recovered"));
-//                    txtActive.setText(jsonObject.getString("active"));
-
+                    txtCases.setText(jsonObject.getString("cases"));
+                    txtDeaths.setText(jsonObject.getString("deaths"));
+                    txtRecovered.setText(jsonObject.getString("recovered"));
+                    txtActive.setText(jsonObject.getString("active"));
+                    tgl.setText(getDate(jsonObject.getLong("updated")));
+                    
                     PieChart pieChart = findViewById(R.id.piechart);
+                    ArrayList<Integer> colors = new ArrayList<Integer>();
+                    for(int c: MY_COLORS) colors.add(c);
                     ArrayList<PieEntry> Produser = new ArrayList<>();
-                    Produser.add(new PieEntry(Float.parseFloat(jsonObject.getString("cases")), "Kasus"));
-                    Produser.add(new PieEntry(Float.parseFloat(jsonObject.getString("deaths")), "Kematian"));
-                    Produser.add(new PieEntry(Float.parseFloat(jsonObject.getString("recovered")), "Sembuh"));
-                    Produser.add(new PieEntry(Float.parseFloat(jsonObject.getString("active")), "Aktif"));
+                    Produser.add(new PieEntry(Float.parseFloat(jsonObject.getString("cases"))));
+                    Produser.add(new PieEntry(Float.parseFloat(jsonObject.getString("deaths"))));
+                    Produser.add(new PieEntry(Float.parseFloat(jsonObject.getString("recovered"))));
+                    Produser.add(new PieEntry(Float.parseFloat(jsonObject.getString("active"))));
                     PieDataSet pieDataSet = new PieDataSet(Produser, "");
-                    pieDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
-                    pieDataSet.setValueTextColor(Color.BLACK);
-                    pieDataSet.setValueTextSize(14f);
+                    pieDataSet.setColors(colors);
+                    pieDataSet.setValueTextSize(0f);
                     PieData pieData = new PieData(pieDataSet);
                     pieChart.setData(pieData);
                     pieChart.getDescription().setEnabled(false);
-                    pieChart.setCenterText("Info Covid");
                     pieChart.animate();
 
                     dialog.cancel();
